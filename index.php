078a4fdad648b41c18c8cdb063f33e9edd30ba67
AxelDuval <axel.duval@gmail.com>
2022-01-06 11:45:02 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+include('templates/header.php');
+include('templates/nav.php');
+
+echo "Page d'accueil";
+
+
+include('templates/footer.php');
---
+++ templates/nav.php
@@ -0,0 +1,29 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Biblio</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="../index.php">Accueil</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Page 1</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Page 2</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Page 3</a>
+        </li>
+        <nav class="nav-item">
+          <a class="nav-link" id="logout" href="#">Connexion / Deconnexion</a>
+        </nav>
+      </ul>
+     
+    </div>
+  </div>
+</nav>
+<main>
