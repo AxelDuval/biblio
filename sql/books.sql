46312b17f8401cc57ba2123c691cc5e961d4809e
AxelDuval <axel.duval@gmail.com>
2022-01-07 11:04:33 +0100

    Access to bdd with the Model class

---
+++ sql/books.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : ven. 07 jan. 2022 à 10:36
+-- Version du serveur : 10.4.22-MariaDB
+-- Version de PHP : 8.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `biblio`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `books`
+--
+
+CREATE TABLE `books` (
+  `id` int(11) NOT NULL,
+  `title` varchar(150) NOT NULL,
+  `author` text NOT NULL,
+  `release_date` date NOT NULL,
+  `category` text NOT NULL,
+  `availablity` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `books`
+--
+
+INSERT INTO `books` (`id`, `title`, `author`, `release_date`, `category`, `availablity`) VALUES
+(1, 'Dom Quichotte', 'Miguel Cervantes', '1605-01-01', 'Roman', 1),
+(2, 'Voyage au bout de la nuit', 'Louis-Ferdinand Céline', '1932-01-01', 'Roman', 0),
+(3, 'Le Petit Prince', 'Antoine de Saint-Exupéry', '1943-01-01', 'Littérature jeunesse', 1),
+(4, 'Les Fleurs du mal', 'Charles Baudelaire', '1857-01-01', 'Littérature Classique', 1);
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `books`
+--
+ALTER TABLE `books`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
